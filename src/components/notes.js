a7d9a9740853663420bfe18006e0275f6f8c0225
fantasticfoo <102485361+fantasticfoo@users.noreply.github.com>
2024-07-27 20:29:55 -0700

    Initial commit

---
+++ src/components/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None",
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "Q. How How to keep a programmer in the shower forever. A. You can't",
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays",
+  },
+  {
+    key: 4,
+    title: "Maps",
+    content:
+      "Q. How did the programmers split their tasks? A. They hash it out.",
+  },
+];
+
+export default notes;
